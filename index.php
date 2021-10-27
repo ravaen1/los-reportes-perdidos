4dfb3ef3ff5bf02b603de4cc7e9cfb500071fce3
rai <ravaen1001@gmail.com>
2021-10-27 15:12:49 -0500

    formulario -bd

--- index.php
+++ index.php
@@ -33,7 +33,18 @@ $resultadocategorias = $conexion->query($sqlcategorias);
           <button type="submit" name="mostrar" class="btn btn-primary my-1">mostrar</button>
 
       </form>
+      <?php
+      if (isset($_POST['mostrar'])) {
+          
+           $categoriaseleccionada = $_POST['cat'];
+           echo'categorias', $categoriaseleccionada;
+
+      } else {
+          echo'error';
+      }
       
+      ?>
+
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
