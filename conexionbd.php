774b73aa5036a88c6be41f3f85657278bb886235
rai <ravaen1001@gmail.com>
2021-10-27 10:14:00 -0500

    correcion 2 bd

--- conexionbd.php
+++ conexionbd.php
@@ -8,8 +8,6 @@ $conexion = new mysqli($server, $user, $pass, $bd);
 if (mysqli_connect_errno()) {
    echo 'no conectado', mysqli_connect_errno();
    exit();
-}else{
-    echo 'no conectado pe';
 }
 
 ?>
