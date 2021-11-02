40c85e5b32ef8ae1e82a5c7c3aed0eae9c585990
rai <ravaen1001@gmail.com>
2021-11-01 21:35:24 -0500

    icono impreora boostrap5

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $resultadocategorias = $conexion->query($sqlcategorias);
           </tbody>
       </table>
 
-      <a class="nav-link" href="#"><i class="fas fa-print"></i>imprimir</a>
+      <a class="nav-link" href="#"><i class="bi bi-printer"></i>imprimir</a>
        
 
        <?php
@@ -98,5 +98,6 @@ $resultadocategorias = $conexion->query($sqlcategorias);
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
   </body>
 </html>
